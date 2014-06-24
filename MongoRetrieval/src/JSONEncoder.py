0c329c0fae93e927a11cf34d721dcee2067ddaf6
erikhalperin <erikhalperin@uchicago.edu>
2014-06-24 16:58:53 -0500

    Added bar graph function

--- MongoRetrieval/src/JSONEncoder.py
+++ MongoRetrieval/src/JSONEncoder.py
@@ -7,7 +7,17 @@ Created on Jun 24, 2014
 import json
 from EfficiencyHistogram import *
 
-#print(json.dumps(data))
+def plotStuff(vals, heights):
+    
+    num_vals = []
+    counter = 0
+    for k in vals:
+        num_vals.append(counter)
+        counter += 1
+    
+    plt.bar(num_vals, heights)
+    plt.xticks(num_vals, vals)
+    plt.show()
 
 def mainJSON(host, port):
     client = MongoClient(host, port)
@@ -18,6 +28,7 @@ def mainJSON(host, port):
     
     site_list = ["ucdavis.edu", "bnl.gov", "smu.edu", "local", "tier2", "unl.edu", "aglt2.org", "iu.edu", "purdue.edu", "uc.mwt2.org", 
                  "northwestern.edu", "nd.edu", "wisc.edu", "buffalo.edu"]
+    days_list = []
     
     for site in site_list:
         time_list = parseList(dbFindItemFromUser("RemoteWallClockTime", None, None, site, coll))
@@ -25,9 +36,12 @@ def mainJSON(host, port):
         for e in time_list:
             tot += e
         days = tot / 60 / 60 / 24
+        days_list.append(days)
         dic = {site: days}
         data[0].update(dic)
+        
+    plotStuff(site_list, days_list)
     
-    print(json.dumps(data))
+    return(json.dumps(data))
     
 mainJSON('mc.mwt2.org', 27017)
