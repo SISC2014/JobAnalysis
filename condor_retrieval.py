409a5f37362f37c15a30722dc0e0781cad1cab6b
erikhalperin <erikhalperin@uchicago.edu>
2014-07-01 11:38:47 -0500

    Added parseconfig
    program currently not working

--- condor_retrieval.py
+++ condor_retrieval.py
@@ -1,55 +1,78 @@
 # Erik Halperin, 6/26/14
-# Polls a Condor collector for some data and outputs it to Mongo
-# collectors: osg-flock.grid.iu.edu, uc3-mgt.mwt2.org, appcloud.uchicago.edu
+# Polls Condor collectors for some data and outputs it to Mongo
+# There must be a config.ini with a [poll] section and the options collectors and intervals, the values of which should be seperated by commas
 # Having a hard time precisely counting jobs processsed per second, but generally a couple thousand jobs won't take more than 3 seconds
 
-import sys, time, argparse
+import sys, time
 import classad, htcondor
-from pymongo import Connection
-from concurrent import futures
+from pymongo import Connection #to connect to mongo
+from concurrent import futures #for processing collectors concurrently
+from ConfigParser import RawConfigParser #for reading from config.ini
 
 #set up mongo collection
 connection = Connection('mc.mwt2.org', 27017)
 db = connection.visualization_db
 dbc = db.condor_records
 
-def mongo_store(coll):
-    slot_state = coll.query(htcondor.AdTypes.Startd,'true',['Name','RemoteGroup','NodeOnline','JobId','State','RemoteOwner','COLLECTOR_HOST_STRING'])
-    timestamp = str(int(time.time()))
+#default wait time interval
+def_int = 30
 
-    #storing data into mongo
-    for slot in slot_state:
-        sl = dict(slot)
-        if 'JobId' in sl:
-            sl['_id'] = sl.pop('JobId') #JobId will become _id for mongo
-        else:
-            continue
-        if 'TargetType' in sl: #TargetType, CurrentTime, and MyType tag along for some reason
-            del sl['TargetType']
-        if 'CurrentTime' in sl:
-            del sl['CurrentTime']
-        if 'MyType' in sl:
-            del sl['MyType']
-        dbc.insert(sl)
+def config_parse():
+    conf = RawConfigParser()
+    conf.read('config.ini')
 
-    return
+    collectors = []
+    intervals = []
+    if conf.has_section('poll'):
+        #get collectors
+        if conf.has_option('poll', 'collectors'):
+            value = conf.get('poll', 'collectors')
+            for v in value.split(','):
+                collectors.append(v.strip())
 
+        #get wait intervals for collectors
+        if conf.has_option('poll', 'intervals'):
+            value = conf.get('poll', 'intervals')
+            for v in value.split(','):
+                intervals.append(int(v.strip()))
+
+    #errors and checking
+    if(len(collectors) != len(intervals)):
+        sys.stderr.write('config.ini: each collector must have a wait interval\n')
+        sys.exit(0)
+
+    new_intvs = [i if i > 0 else def_int for i in intervals]
+
+    return [collectors, new_intvs]
+
+def mongo_store(coll, intv):
+    while(True):
+        slot_state = coll.query(htcondor.AdTypes.Startd,'true',['Name','RemoteGroup','NodeOnline','JobId','State','RemoteOwner','COLLECTOR_HOST_STRING'])
+        timestamp = str(int(time.time()))
+
+        #storing data into mongo
+        for slot in slot_state:
+            sl = dict(slot)
+            if 'JobId' in sl:
+                sl['_id'] = sl.pop('JobId') #JobId will become _id for mongo
+            else:
+                continue
+            if 'TargetType' in sl: #TargetType, CurrentTime, and MyType tag along for some reason
+                del sl['TargetType']
+            if 'CurrentTime' in sl:
+                del sl['CurrentTime']
+            if 'MyType' in sl:
+                del sl['MyType']
+            dbc.insert(sl)
+
+        time.sleep(interval)
+        
 def main():
-    while(1):
-        parser = argparse.ArgumentParser(description="Poll HTCondor collector for information and store it in mongo")
-        parser.add_argument("time", help="time to wait between polling")
-        args = parser.parse_args()
+    ret_list = config_parse() #collectors = ret_list[0], intervals = ret_list[1]
 
-        st = float(time.time())
-        #not sure if using futures correctly, idea is to run 3 mongo_stores concurrently
-        with futures.ThreadPoolExecutor(max_workers=3) as executor:
-            executor.submit(mongo_store, htcondor.Collector('osg-flock.grid.iu.edu'))
-            executor.submit(mongo_store, htcondor.Collector('uc3-mgt.mwt2.org'))
-            executor.submit(mongo_store, htcondor.Collector('appcloud.uchicago.edu'))
-        et = float(time.time())
-
-        print("Total time: ", et - st)
-
-        time.sleep(float(args.time))
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        for collector, interval in zip(ret_list[0],ret_list[1]):
+            executor.submit(mongo_store, htcondor.Collector(collector), interval)
 
 main()
+
