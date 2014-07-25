d51a515122343c6275947e2f8e04f268a08d5f2e
erikhalperin <erikhalperin@uchicago.edu>
2014-07-25 13:28:51 -0500

    added configurable options specified in config.ini
    classads, mongo url, mongo port, mongo database, and mongo collector

--- condor_retrieval.py
+++ condor_retrieval.py
@@ -5,18 +5,12 @@
 # End program by keyboard interrupt (ctrl-c)
 
 import sys, time, signal
-import classad, htcondor
-from pymongo import Connection # to connect to mongo
+import classad, htcondor # for retrieving from condor collectors
+from pymongo import MongoClient # to connect to mongo
+from pymongo import database # access mongo database
+from pymongo import collection # access mongo collection
 from concurrent import futures # for processing collectors concurrently
 from ConfigParser import RawConfigParser # for reading from config.ini
-
-# set up mongo collection
-connection = Connection('db.mwt2.org', 27017)
-db = connection.visualization_db
-dbc = db.condor_records
-
-# default wait time interval
-def_int = 30
 
 # main thread changes this to signal child threads to exit
 exit_flag = False
@@ -25,39 +19,74 @@ def config_parse():
     conf = RawConfigParser()
     conf.read('config.ini')
 
-    collectors = []
-    intervals = []
-    if conf.has_section('poll'):
+    collectors, intervals = [], []
+    try:
         # get collectors
-        if conf.has_option('poll', 'collectors'):
-            value = conf.get('poll', 'collectors')
-            for v in value.split(','):
-                collectors.append(v.strip())
-
+        value = conf.get('poll', 'collectors')
+        for v in value.split(','):
+            collectors.append(v.strip())
         # get wait intervals for collectors
-        if conf.has_option('poll', 'intervals'):
-            value = conf.get('poll', 'intervals')
-            for v in value.split(','):
-                intervals.append(int(v.strip()))
+        value = conf.get('poll', 'intervals')
+        for v in value.split(','):
+            intervals.append(int(v.strip()))
+    except Exception:
+        sys.stderr.write('config.ini: must specify condor collectors and collector wait intervals\n')
+        sys.exit(0)
+
+    classad_list = []
+    # get classads
+    try:
+        value = conf.get('poll', 'classads')
+        for v in value.split(','):
+            classad_list.append(v.strip())
+    except Exception:
+        sys.stderr.write('config.ini: must specify classads to be queried\n')
+        sys.exit(0)
+
+    try:
+        # get collection info
+        url = conf.get('mongo', 'url')
+        port = int(conf.get('mongo', 'port'))
+        db_str = conf.get('mongo', 'database')
+        m_coll_str = conf.get('mongo', 'collection')
+    except Exception:
+        sys.stderr.write('config.ini: must specifiy mongo url, port, database, and collection\n')
+        sys.exit(0)
+
+    try:
+        # set up mongo collection
+        client = MongoClient(url, port)
+        db = database.Database(client, db_str)
+        # Note: m_coll is mongo collection, coll is condor collector
+        m_coll = collection.Collection(db, m_coll_str)
+    except Exception, e:
+        sys.stderr.write('could not connect to mongo collector, check url and port\n')
+        print(str(e))
+        sys.exit(0)
 
     # make sure each collector has an interval
     if(len(collectors) != len(intervals)):
         sys.stderr.write('config.ini: each collector must have a wait interval\n')
         sys.exit(0)
 
-    # make sure each interval is a positive number
-    new_intvs = [i if i > 0 else def_int for i in intervals]
+    # make sure each intervals is an integer greater than 0
+    for intv in intervals:
+        if intv >= 0:
+            continue
+        else:
+            sys.stderr.write('config.ini: each wait interval must be an integer greater than 0')
+            sys.exit(0)
 
-    return [collectors, new_intvs]
+    return [collectors, intervals, m_coll, classad_list]
 
-def mongo_store(coll, intv):
+def mongo_store(coll, intv, m_coll, classads):
     while(True):
-        # gets each job running on condor rather than ones created since last poll - this needs to be changed
-        slot_state = coll.query(htcondor.AdTypes.Startd,'true',['Name','RemoteGroup','NodeOnline','JobId','State','RemoteOwner','COLLECTOR_HOST_STRING'])
-        timestamp = str(int(time.time())) # don't have a use for this yet
-
         if exit_flag is True:
             sys.exit()
+
+        # gets each job running on condor rather than ones created since last poll - this needs to be changed
+        slot_state = coll.query(htcondor.AdTypes.Startd, 'true', classads)
+        timestamp = str(int(time.time())) # don't have a use for this yet
 
         # storing data into mongo
         for slot in slot_state:
@@ -67,32 +96,42 @@ def mongo_store(coll, intv):
                 sl['_id'] = sl.pop('JobId')
             else:
                 continue
-            # Remove TargetType, CurrentTime, and MyType
-            if 'TargetType' in sl:
-                del sl['TargetType']
+
+            # The classads TargetType, CurrentTime, and MyType always tag along for some reason
+            # CurrentTime yields time() which mongo can't parse, so it must be removed -- we can leave the rest
             if 'CurrentTime' in sl:
                 del sl['CurrentTime']
+            if 'TargetType' in sl:
+                del sl['TargetType']
             if 'MyType' in sl:
                 del sl['MyType']
-            dbc.insert(sl)
+
+            try:
+                # use upsert to update existing jobs in mongo if they're still in the collector
+                jobid = sl['_id']
+                del sl['_id']
+                m_coll.update( { '_id': jobid }, { '$set': sl }, upsert=True)
+            except Exception, e:
+                print(str(e))
+                sys.exit(0)
 
         time.sleep(intv)
 
 def main():
-    ret_list = config_parse() # collectors = ret_list[0], intervals = ret_list[1]
+    ret_list = config_parse() # collectors = ret_list[0], intervals = ret_list[1], collection = ret_list[2], classads = ret_list[3]
     global exit_flag
 
     # process each collector in a thread running concurrently with the other threads
     with futures.ThreadPoolExecutor(max_workers=len(ret_list[1])) as executor:
         for collector, interval in zip(ret_list[0],ret_list[1]):
-            executor.submit(mongo_store, htcondor.Collector(collector), interval)
+            executor.submit(mongo_store, htcondor.Collector(collector), interval, ret_list[2], ret_list[3])
         while True:
             # pause indefinitely, waiting for SIGINT
             try:
                 signal.pause()
             except KeyboardInterrupt:
                 break
-            except:
+            except Exception:
                 pass
 
         # once out of infinite loop, end child threads
